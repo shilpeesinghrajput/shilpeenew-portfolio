b1ff917be6a5db1c9247d741cdcb46960f3aaba9
Shilpee singh <your-github-shilpeesingh336@gmail.com>
2026-07-05 00:11:14 +0530

    Remove unnecessary file

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,10 +32,6 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);
 
-
-    console.log("Service:", import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
-console.log("Template:", import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID);
-console.log("Public Key:", import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
